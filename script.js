b7db0b3f16c6095ef72b9d068e0244c23e74d9cc
meow56 <spinattacktotheface@gmail.com>
2019-05-19 22:37:28 -0500

    Expanding
    More problems, but nothing that a little Googling can't fix.

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 /*
 First off:
 how do tree
+  create object class node with parent
 
+Second:
+how do display
+  abuse border-radius
 */
 
-function Node(parent) {
+var xOffset = 10; // how far to the right the first node is
+var yOffset = 10; // how far down the first node is
+var genY = 100; // height difference between generations
+
+function Tree(nodes) {
+  this.nodes = nodes;
+  this.display = function() {
+    for(var i = 0; i < this.nodes.length; i++) {
+      this.nodes[i].display();
+    }
+  }
+}
+
+function Node(parent, link, official) {
   this.par = parent;
+  this.link = link;
+  this.off = official;
   this.gen = parent.gen ? parent.gen : 0;
+  this.display = function() {
+    if(this.off) {
+      this.x = xOffset;
+      this.y = yOffset + (genY * this.gen);
+      var nodeDiv = document.createElement("div");
+    }
+  }
 }
