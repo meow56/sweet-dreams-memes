ae7a520dbcb6769f805f1f1a20e4c8e57c6b1536
meow56 <spinattacktotheface@gmail.com>
2019-05-20 10:23:17 -0500

    Update script.js
    Time to mess with SVG a little

--- script.js
+++ script.js
@@ -16,21 +16,50 @@ function Tree(nodes) {
   this.nodes = nodes;
   this.display = function() {
     for(var i = 0; i < this.nodes.length; i++) {
-      this.nodes[i].display();
+      this.nodes[i].display(this.nodes, i);
     }
   }
 }
 
-function Node(parent, link, official) {
+function Node(parent, metadata, official) {
   this.par = parent;
-  this.link = link;
-  this.off = official;
+  this.meta = metadata; // object that holds stuff like the name, date, description, link
+  this.off = official || false;
   this.gen = parent.gen ? parent.gen : 0;
-  this.display = function() {
+  this.display = function(nodes, i) {
     if(this.off) {
       this.x = xOffset;
-      this.y = yOffset + (genY * this.gen);
-      var nodeDiv = document.createElement("div");
+    } else {
+      var newNodes = [];
+      for(var j = 0; j < nodes.length; j++) {
+        if(nodes[j].gen === this.gen) {
+          newNodes.push(nodes[j]);
+        }
+      }
+      this.x = ((document.getElementById("mainDiv").width - xOffset) * i / newNodes.length) + xOffset;
+    }
+    this.y = yOffset + (genY * this.gen);
+    /* Update to SVG later
+    var nodeDiv = document.createElement("div");
+    nodeDiv.id = this.meta.name;
+    nodeDiv.borderRadius = 50%;
+    nodeDiv.backgroundPosition = "left " + this.x + "px top " + this.y + "px";
+    document.getElementById("mainDiv").appendChild(nodeDiv);
+    */
+  }
+}
+
+function setup() {
+  var svg = document.createElement("svg");
+  svg.id = "mainSVG";
+  
+  // please define mainTree here
+  
+  var highGen = 0;
+  for(var i = 0; i < mainTree.nodes.length; i++) {
+    if(mainTree.nodes[i].gen > highGen) {
+      highGen = mainTree.nodes[i].gen;
     }
   }
+  svg.viewbox = "0 0 " + (document.getElementById("mainDiv").width - xOffset) + " " + ((highGen + 1) * genY);
 }
