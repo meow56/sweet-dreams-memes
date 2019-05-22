e3b3d6bb0c6a867f5e908bd0041b2ae74d1f9b7b
meow56 <spinattacktotheface@gmail.com>
2019-05-22 10:40:38 -0500

    Update script.js
    good luck, little pupper

--- script.js
+++ script.js
@@ -66,14 +66,17 @@ function Node(metadata, official) {
       this.x = ((svg.getAttributeNS(null, "width") - xOffset) * (position + 1) / (newNodes.length + 1)) + xOffset;
     }
     this.y = yOffset + (genY * this.gen);
+    var newLink = document.createElementNS("http://www.w3.org/2000/svg", "a");
+    newLink.setAttributeNS(null, "href", this.meta.link);
     var newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     newCircle.setAttributeNS(null, "id", this.meta.name);
     newCircle.setAttributeNS(null, "r", "" + nodeRadius);
     newCircle.setAttributeNS(null, "cx", "" + this.x);
     newCircle.setAttributeNS(null, "cy", "" + this.y);
-    newCircle.addEventListener("mouseover", new Function("var newTri = document.createElementNS(\"http://www.w3.org/2000/svg\", \"polygon\"); newTri.setAttributeNS(null, \"id\", \"" + this.meta.name + "hovertri\"); newTri.setAttributeNS(null, \"points\", (" + this.x + " + nodeRadius + 5) + \",\" + " + this.y + " + \" \" + (" + this.x + " + nodeRadius + 5 + 20) + \",\" + " + this.y + " + \" \" + (" + this.x + " + nodeRadius + 5 + 20) + \",\" + (" + this.y + " + 20)); newTri.setAttributeNS(null, \"fill\", \"white\"); newTri.setAttributeNS(null, \"stroke\", \"black\"); /* change to none when backgrounds implemented */ svg.appendChild(newTri); var newRect = document.createElementNS(\"http://www.w3.org/2000/svg\", \"rect\"); newRect.setAttributeNS(null, \"id\", \"" + this.meta.name + "hoverrect\"); newRect.setAttributeNS(null, \"x\", \"\" + (" + this.x + " + nodeRadius + 5 + 20)); newRect.setAttributeNS(null, \"y\", \"\" + " + this.y + "); newRect.setAttributeNS(null, \"height\", \"100\"); newRect.setAttributeNS(null, \"width\", \"200\"); newRect.setAttributeNS(null, \"fill\", \"white\"); newRect.setAttributeNS(null, \"stroke\", \"black\"); /* change to none when background implemented*/ svg.appendChild(newRect);"));
-    newCircle.addEventListener("mouseout", new Function("document.getElementById(\"" + this.meta.name + "hovertri\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hovertri\")); document.getElementById(\"" + this.meta.name + "hoverrect\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hoverrect\"));"));
-    svg.appendChild(newCircle);
+    newCircle.addEventListener("mouseover", new Function("var newTri = document.createElementNS(\"http://www.w3.org/2000/svg\", \"polygon\"); newTri.setAttributeNS(null, \"id\", \"" + this.meta.name + "hovertri\"); newTri.setAttributeNS(null, \"points\", (" + this.x + " + nodeRadius + 5) + \",\" + " + this.y + " + \" \" + (" + this.x + " + nodeRadius + 5 + 20) + \",\" + " + this.y + " + \" \" + (" + this.x + " + nodeRadius + 5 + 20) + \",\" + (" + this.y + " + 20)); newTri.setAttributeNS(null, \"fill\", \"white\"); newTri.setAttributeNS(null, \"stroke\", \"black\"); /* change to none when backgrounds implemented */ svg.appendChild(newTri); var newRect = document.createElementNS(\"http://www.w3.org/2000/svg\", \"rect\"); newRect.setAttributeNS(null, \"id\", \"" + this.meta.name + "hoverrect\"); newRect.setAttributeNS(null, \"x\", \"\" + (" + this.x + " + nodeRadius + 5 + 20)); newRect.setAttributeNS(null, \"y\", \"\" + " + this.y + "); newRect.setAttributeNS(null, \"height\", \"100\"); newRect.setAttributeNS(null, \"width\", \"200\"); newRect.setAttributeNS(null, \"fill\", \"white\"); newRect.setAttributeNS(null, \"stroke\", \"black\"); /* change to none when background implemented*/ svg.appendChild(newRect);var title = document.createElementNS(\"http://www.w3.org/2000/svg\", \"text\");title.setAttributeNS(null, \"id\", \"" + this.meta.name + "hovertitle\");title.setAttributeNS(null, \"x\", \"\" + " + this.x + " + nodeRadius + 5 + 20);title.setAttributeNS(null, \"y\", \"\"" + this.y + " + 20);title.setAttributeNS(null, \"style\", \"font-size: 20px;\");title.setAttributeNS(null, \"fill\", \"black\");title.textContent = \"" + this.meta.name + "\";svg.appendChild(title);var dateline = document.createElementNS(\"http://www.w3.org/2000/svg\", \"text\");dateline.setAttributeNS(null, \"id\", \"" + this.meta.name + "hoverbyline\");title.setAttributeNS(null, \"x\", \"\" + " + this.x + " + nodeRadius + 5 + 20);title.setAttributeNS(null, \"y\", \"\"" + this.y + " + 30);dateline.setAttributeNS(null, \"style\", \"font-size: 10px;\");dateline.setAttributeNS(null, \"fill\", \"grey\");dateline.textContent = \"Created " + this.meta.date + ", by " + this.meta.by + "\";svg.appendChild(dateline);var desc = document.createElementNS(\"http://www.w3.org/2000/svg\", \"text\");desc.setAttributeNS(null, \"id\", \"" + this.meta.name + "hoverdesc\");title.setAttributeNS(null, \"x\", \"\" + " + this.x + " + nodeRadius + 5 + 20);title.setAttributeNS(null, \"y\", \"\"" + this.y + " + 42);desc.setAttributeNS(null, \"style\", \"font-size: 12px;\");desc.setAttributeNS(null, \"fill\", \"black\");desc.textContent = \"" + this.meta.desc + "\";svg.appendChild(desc);svg.appendChild(newRect);"));
+    newCircle.addEventListener("mouseout", new Function("document.getElementById(\"" + this.meta.name + "hovertri\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hovertri\")); document.getElementById(\"" + this.meta.name + "hovertitle\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hovertitle\")); document.getElementById(\"" + this.meta.name + "hoverbyline\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hoverbyline\")); document.getElementById(\"" + this.meta.name + "hoverdesc\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hoverdesc\")); document.getElementById(\"" + this.meta.name + "hoverrect\").parentNode.removeChild(document.getElementById(\"" + this.meta.name + "hoverrect\"));"));
+    newLink.appendChild(newCircle);
+    svg.appendChild(newLink);
   }
 }
 
@@ -83,10 +86,12 @@ function setup() {
   } else {
     
     mainTree = new Tree([new Node({name: "owo", 
+                                   by: "epic meeeeem",
                                    date: "05/20/40", 
                                    desc: "When the world is ravaged by furries, the only savior of humanity will be one of those cat spritzers.", 
                                    link: "sdfsdf"}),
                          new Node({name: "strongeset",
+                                   by: "STRONGESET!!!!",
                                    date: "09/09/09",
                                    desc: "EYE'M THA STRONGESET!!!",
                                    link: "fjofojfojF"})
@@ -104,14 +109,14 @@ function setup() {
       }
     }
     svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttributeNS(null, "width", "" + (document.getElementById("mainDiv").offsetWidth + 2 * nodeRadius));
+    svg.setAttributeNS(null, "width", "" + (document.getElementById("mainDiv").offsetWidth));
     svg.setAttributeNS(null, "height", "" + ((highGen + 2) * genY + 2 * nodeRadius));
     for(var i = 0; i < highGen + 1; i++) {
       var newLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
       newLine.setAttributeNS(null, "x1", "0");
-      newLine.setAttributeNS(null, "y1", "" + (i * genY));
+      newLine.setAttributeNS(null, "y1", "" + (i * genY + yOffset));
       newLine.setAttributeNS(null, "x2", "" + svg.getAttributeNS(null, "width") - (2 * nodeRadius));
-      newLine.setAttributeNS(null, "y2", "" + (i * genY));
+      newLine.setAttributeNS(null, "y2", "" + (i * genY + yOffset));
       newLine.setAttributeNS(null, "stroke", "grey");
       svg.appendChild(newLine);
       // create SVG elements
